73caf5a54cbb803d4e26457e8a644d5fef9c54f6
Chris <34993144+Mesiow@users.noreply.github.com>
2020-10-20 12:59:01 -0400

    commit before graphic lib change

--- Wire World/Wire_World.cpp
+++ Wire World/Wire_World.cpp
@@ -3,13 +3,12 @@
 
 Wire_World::Wire_World()
 {
-	cells_ = new Cell[width * height];
+	//cells_ = new Cell[width * height];
+	cells_.resize(width * height);
 	srand(time(0));
+
 	//init cells
 	for (int i = 0; i < width * height; i++) {
-		//int randState = rand() % (int)State::CONDUCTOR + 1; //0 - 3
-		//std::cout << randState << std::endl;
-		//cells_[i].state = (State)(randState);
 		cells_[i].state = State::EMPTY;
 		cells_[i].rect = sf::RectangleShape(sf::Vector2f(cellSize_, cellSize_));
 	}
@@ -40,6 +39,8 @@ Wire_World::~Wire_World()
 
 void Wire_World::render(sf::RenderTarget* target)
 {
+	if(started_)
+		runRules();
 	for (int i = 0; i < width * height; i++) {
 		target->draw(cells_[i].rect);
 	}
@@ -48,6 +49,21 @@ void Wire_World::render(sf::RenderTarget* target)
 void Wire_World::pollInput(sf::RenderWindow &window, sf::Event &ev)
 {
 	switch (ev.type) {
+		case sf::Event::KeyReleased: {
+			if (ev.key.code == key::Space)
+				started_ = !started_; //start simulation
+
+			if (ev.key.code == key::Q) { //Electron head selected
+				switchSelection_ = !switchSelection_;
+				if (switchSelection_)
+					selectedCell_ = State::HEAD;
+				else
+					selectedCell_ = State::CONDUCTOR;
+				std::cout << "Switched to " << (int)selectedCell_ << std::endl;
+			}
+		}
+		break;
+
 		case sf::Event::MouseButtonReleased:{
 			sf::Mouse::Button button = ev.mouseButton.button;
 
@@ -66,24 +82,32 @@ void Wire_World::pollInput(sf::RenderWindow &window, sf::Event &ev)
 			
 			//Left mouse button released
 			if(button == sf::Mouse::Left){
-				//if click counter greater or equal to 2, call placeConductorCellsFrom
-				clickCounter++;
-				//place single conductor cell
-				if (clickCounter % 2 != 0) {
-					startX = posX;
-					startY = posY;
-					placeConductorCell(posX, posY);
-				} //every 2nd click
-				else if (clickCounter % 2 == 0) { //place multiple cells from start position x to end position x
-					clickCounter = 0;
-					endX = posX;
-					endY = posY;
-					placeConductorCellsFrom(startX, endX, startY, endY);
+				//Conductor cell selected
+				if (selectedCell_ == State::CONDUCTOR) {
+					//if click counter greater or equal to 2, call placeConductorCellsFrom
+					clickCounter++;
+					//place single conductor cell
+					if (clickCounter % 2 != 0) {
+						startX = posX;
+						startY = posY;
+						placeCell(posX, posY);
+					} //every 2nd click
+					else if (clickCounter % 2 == 0) { //place multiple cells from start position x to end position x
+						clickCounter = 0;
+						endX = posX;
+						endY = posY;
+						placeConductorCellsFrom(startX, endX, startY, endY);
+					}
+				}
+				else if (selectedCell_ == State::HEAD) {
+					placeCell(posX, posY);
 				}
 			}
 			//Right mouse button released
 			else if (button == sf::Mouse::Right) {
-				removeConductorCell(posX, posY);
+				if (selectedCell_ == State::CONDUCTOR) {
+					removeCell(posX, posY);
+				}
 			}
 		}
 		break;
@@ -93,7 +117,6 @@ void Wire_World::pollInput(sf::RenderWindow &window, sf::Event &ev)
 void Wire_World::setCellColor(Cell& cell, State state)
 {
 	sf::Color color;
-
 	switch (state) {
 		case State::EMPTY: color = sf::Color::Black; break;
 		case State::HEAD: color = sf::Color::Blue; break;
@@ -103,7 +126,14 @@ void Wire_World::setCellColor(Cell& cell, State state)
 	cell.rect.setFillColor(color);
 }
 
-void Wire_World::placeConductorCell(int x, int y)
+void Wire_World::setCellColor(int x, int y, State state)
+{
+	int index = (y * width + x);
+	cells_[index].state = state;
+	setCellColor(cells_[index], cells_[index].state);
+}
+
+void Wire_World::placeCell(int x, int y)
 {
 	//convert to grid space to index proper cell in array
 	int gridX = (x / cellSize_);
@@ -114,7 +144,12 @@ void Wire_World::placeConductorCell(int x, int y)
 	if (index >= width * height)
 		index = width * height;
 
-	cells_[index].state = State::CONDUCTOR;
+	//place correct cell
+	if (selectedCell_ == State::CONDUCTOR)
+		cells_[index].state = State::CONDUCTOR;
+	else if (selectedCell_ == State::HEAD)
+		cells_[index].state = State::HEAD;
+
 	setCellColor(cells_[index], cells_[index].state);
 }
 
@@ -161,7 +196,7 @@ void Wire_World::placeConductorCellsFrom(int startX, int endX, int startY, int e
 	
 }
 
-void Wire_World::removeConductorCell(int x, int y)
+void Wire_World::removeCell(int x, int y)
 {
 	//convert to grid space to index proper cell in array
 	int gridX = (x / cellSize_);
@@ -174,4 +209,111 @@ void Wire_World::removeConductorCell(int x, int y)
 
 	cells_[index].state = State::EMPTY;
 	setCellColor(cells_[index], cells_[index].state);
+}
+
+void Wire_World::setCell(int x, int y, Cell cell)
+{
+	int index = (y * width + x);
+	Cell& c = cells_[index];
+	c = cell;
+	/*c.posX = (x * cellSize_);
+	c.posY = (y * cellSize_);
+	c.rect.setPosition(sf::Vector2f(c.posX, c.posY));*/
+	setCellColor(x, y, c.state);
+}
+
+Position Wire_World::getGridPosition(int x, int y)
+{
+	Position p;
+	int gridX = (x / cellSize_);
+	int gridY = (y / cellSize_);
+
+	p.x = gridX;
+	p.y = gridY;
+	return p;
+}
+
+void Wire_World::runRules()
+{
+	std::vector<Cell> newCells(width * height, Cell(State::EMPTY,
+		sf::RectangleShape(sf::Vector2f(cellSize_, cellSize_)), 0, 0));
+	//std::vector<Cell> newCells(width * height);
+
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+
+			int index = (y * width + x);
+			auto& cell = cells_[index];
+			auto& newCell = newCells[index];
+
+			switch (cell.state) {
+			case State::EMPTY:
+				newCell.state = State::EMPTY;
+				break;
+
+				//Electron head - turns into an electron tail
+			case State::HEAD:
+				newCell.state = State::TAIL;
+				break;
+
+				//Electron tail - turns into a conductor
+			case State::TAIL:
+				newCell.state = State::CONDUCTOR;
+				break;
+
+				//Conductor - turns into an electron head if 1 or 2 neighbors is an electron head
+			case State::CONDUCTOR: {
+				int headCount = 0;
+				//loop over all neighbors
+				for (int lx = -1; lx <= 1; lx++) {
+					for (int ly = -1; ly <= 1; ly++) {
+						int nx = lx + x;
+						int ny = ly + y;
+
+						//make sure in range
+						if (nx == -1 || nx == width ||
+							ny == -1 || ny == height ||
+							(lx == 0 && ly == 0)) {
+							continue;
+						}
+						
+						State neighbor = getNeighbor(nx, ny);
+						if (neighbor == State::HEAD)
+							headCount++;
+
+						/*int index = (ny * width + nx);
+						auto cell = cells_[index];
+
+						if (cell.state == State::HEAD)
+							headCount++;*/
+					}
+				}
+				if (headCount > 0) { std::cout << "head count: " << headCount << std::endl; }
+				if (headCount == 1 || headCount == 2) { //turn into electron head
+					newCell.state = State::HEAD;
+				}
+				else
+					newCell.state = State::CONDUCTOR;
+				break;
+			}
+
+			default:
+				break;
+			}
+			//setCell(x, y, newCell);
+			setCellColor(x, y, newCell.state);
+		}
+	}
+	for (auto& cell : newCells) {
+		setCell(cell.posX, cell.posY, cell);
+	}
+}
+
+State Wire_World::getNeighbor(int x, int y)
+{
+	int index = (y * width + x);
+	if (index >= width * height)
+		index = width * height;
+
+	return cells_[index].state;
 }
--- Wire World/Wire_World.h
+++ Wire World/Wire_World.h
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <random>
 #include <array>
+#include <vector>
 
 enum class State {
 	EMPTY = 0, //Black
@@ -12,9 +13,21 @@ enum class State {
 };
 
 struct Cell {
+	Cell(){}
+	Cell(State st, sf::RectangleShape rect, int x, int y){
+		this->state = st;
+		this->rect = rect;
+		this->posX = x;
+		this->posY = y;
+		this->rect.setPosition(x, y);
+	}
 	State state;
 	sf::RectangleShape rect;
 	int posX, posY; //x and y position in screen
+};
+
+struct Position {
+	int x, y;
 };
 
 //input typedefs
@@ -32,21 +45,38 @@ public:
 private:
 	//set cell color based on state
 	void setCellColor(Cell &cell, State state);
-	void placeConductorCell(int x, int y);
+	//set cell color based on position and state
+	void setCellColor(int x, int y, State state);
+	void placeCell(int x, int y);
 	//place conductor cells from startX to endX at current Y position clicked on
 	void placeConductorCellsFrom(int startX, int endX, int startY, int endY);
 	//sets cell to empty
-	void removeConductorCell(int x, int y);
+	void removeCell(int x, int y);
+	void setCell(int x, int y, Cell cell);
+	Position getGridPosition(int x, int y);
+
+	//rule functions
+	void runRules();
+	//get state of a neighbor
+	State getNeighbor(int x, int y);
+
 
 private:
-	//allows for 16 x 16 cells
-	static constexpr int width = 80;
-	static constexpr int height = 45;
-	Cell *cells_;
-	int cellSize_ = 16; //16x16 cell size
+	//allows for 10 x 10 cells
+	//divide screen width by 10(cell size) to get grid width(128) and height(72)
+	static constexpr int width = 128;
+	static constexpr int height = 72;
+	std::vector<Cell> cells_;
+	int cellSize_ = 10; //10x10 cell size
 
 	int clickCounter = 0;
 	
+	//Conductor line
 	int startX = 0, endX = 0; //start x and end x of a conductor line
-	int startY = 0, endY = 0; 
+	int startY = 0, endY = 0;
+
+	//once we place an electron head on a conductor, start the simulation (run the rules)
+	bool started_ = false;
+	bool switchSelection_ = true;
+	State selectedCell_ = State::CONDUCTOR; //current cell to place down
 };
